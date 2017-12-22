e4b735988f636da1900d29556f64f1c341d80657
Leonardo Azocar <leonardoazocar23@gmail.com>
2017-12-21 23:32:34 -0400

    scripts de base de datos

--- scripts.sql
+++ scripts.sql
@@ -1,18 +1,21 @@
-create database descargasBD;
+create database proyredes2;
 
 create table cliente(
-	int cli_id,
-	character varying cli_nombre,
-	int cli_puerto,
-	character varying cli_ip,
-	int cli_num_descargas;
+	cli_id serial,
+	cli_nombre character varying,
+	cli_puerto integer,
+	cli_ip character varying,
+	cli_num_descargas integer,
 	constraint pk_cliente primary key (cli_id)
 );
 
 create table video(
-	int vid_id,
-	character varying vid_nombre;
-	character varying vid_ruta;
-	character varying vid_tamano;
-	int vid_num_descargados;
+    vid_id serial,
+    vid_nombre character varying,
+    vid_ruta character varying,
+    vid_tamano character varying,
+    vid_num_descargados integer,
+    fk_usuario integer,
+    constraint pk_video primary key (vid_id),
+    constraint fk_usuario foreign key (fk_usuario) references cliente(cli_id)
 );
